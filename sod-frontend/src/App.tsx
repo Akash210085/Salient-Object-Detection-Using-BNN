0c0880759708bf975851fde4feb467904867d415
Akash Kumar <kakash21@iitk.ac.in>
2025-04-13 08:46:21 +0530

    testing2

--- sod-frontend/src/App.tsx
+++ sod-frontend/src/App.tsx
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
-
 import { ImageUp } from 'lucide-react';
 import HashLoader from "react-spinners/HashLoader";
 export default function App() {
